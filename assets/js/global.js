0ff7a7c53a41fdb7832eeec39b1ad35ef5e61876
Manxtodd here! <81454215+mann-x@users.noreply.github.com>
2021-07-09 22:58:19 +0700

    Create global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -3,14 +3,14 @@ function welcomeMsg() {
     let array = 0;
 
     const takapedia = `<a href="https://msha.ke/takapedia/" target="_blank">takapedia</a>`;
-    const takapediaIG = `<a href="https://www.instagram.com/taka_pedia/" target="_blank">taka_pedia</a>`;
-    const takapediaWA = `<a href="https://api.whatsapp.com/send?phone=6287843920288" target="_blank">takapedia</a>`;
-    const linkTiktokGua = `<a href="https://vt.tiktok.com/ZSJQ5XuUu/" target="_blank">tiktok</a>`
+    const IG = `<a href="https://www.instagram.com/manxtodd/" target="_blank">manxtodd</a>`;
+    const WA = `<a href="https://api.whatsapp.com/send?phone=6288231715470?text=Bang+Bagi+Bokep" target="_blank">whatsapp</a>`;
+    const githubGw = `<a href="https://github.com/mann-x/" target="_blank">github</a>`
 
-    const msg1 = `<p class="animation mb-0 text-truncate">JOKI, TOP UP & GIFT SKIN ML di ${takapedia} ya</p>`;
-    const msg2 = `<p class="animation mb-0 text-truncate">IG: ${takapediaIG} / WA: ${takapediaWA}</p>`;
-    const msg3 = `<p class="animation mb-0 text-truncate">Takapedia dijamin amanah dan terpercaya</p>`;
-    const msg4 = `<p class="animation mb-0 text-truncate">Follow ${linkTiktokGua} gw ya</p>`;
+    const msg1 = `<p class="animation mb-0 text-truncate">JOKI, TOP UP & GIFT SKIN ML di ${takapedia} ya [StevenJohns]</p>`;
+    const msg2 = `<p class="animation mb-0 text-truncate">Hubungi Gw Di IG: ${IG} / WA: ${WA}</p>`;
+    const msg3 = `<p class="animation mb-0 text-truncate">Inget! WR Sebatas Angka Ya.</p>`;
+    const msg4 = `<p class="animation mb-0 text-truncate">Follow ${githubGw} gw juga ya</p>`;
     const msg5 = `<p class="animation mb-0 text-truncate">Jangan lupa follow sosmed gua dibawah ya</p>`;
 
     let arrayEl = [msg1, msg2, msg3, msg4, msg5];
